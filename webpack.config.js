b456844a2884ca410d93aaa3390304e00d420a6c
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-04-24 20:03:52 -0500

    Uso de asset/resource para imagenes

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,10 @@ module.exports = {
                     "stylus-loader",
                 ],
             },
+            {
+                test: /\.png/,
+                type: "asset/resource",
+            },
         ],
     },
     plugins: [
