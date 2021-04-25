157a2b04fc96d962006c10429b6fbfe91789262a
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-04-25 15:00:58 -0500

    Cambio de redes y cosas del proyecto

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -26,13 +26,13 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
+          <a href="https://twitter.com/geaglts" target="_blank">
             <img src="${twitter}" />
           </a>
-          <a href="https://github.com/gndx">
+          <a href="https://github.com/geaglts" target="_blank">
             <img src="${github}" />
           </a>
-          <a href="https://instagram.com/gndx">
+          <a href="https://instagram.com/geaglts" target="_blank">
             <img src="${instagram}" />
           </a>
         </div>
